2b277f81c1738311bfeb86ed7de7a2d9e83e091c
react-space-coder <anushamddeshpande@gmail.com>
2025-04-23 23:31:33 +0530

    Day 3 learning

--- App.js
+++ App.js
@@ -1,27 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child" },
-    [
-      React.createElement("h1", { id: "subchild" }, "Hi i am h1 tag"),
-      React.createElement("h2", { id: "subchild2" }, "Hi i am h2 tag"),
-    ],
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", { id: "subchild" }, "Hi i am h1 tag"),
-      React.createElement("h2", { id: "subchild2" }, "Hi i am h2 tag"),
-    ])
-  ),
-]);
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello from react js"
-);
+const Titile = () => {
+  return <h1>HI I am Deshpande</h1>;
+};
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Titile />
+      <h1>Hi i am Anusha</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
-console.log(parent);
+root.render(<HeaderComponent />);
